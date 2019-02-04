7d2a37e73aa7df998c19805818deb3b5b5c1355a
Aidan O'Brien <obrienam@appstate.edu>
2019-02-03 21:21:03 -0500

    Worked more on the first demo

--- Demo_Progs/feedtest1.py
+++ Demo_Progs/feedtest1.py
@@ -1,14 +1,20 @@
 import feedparser
+import time
 def newstest(feed, spot):
 	d = feedparser.parse(feed)
-	print "Feed title: " + d['feed']['title']
-	print "Number of articles: " + str(len(d['entries']))
-	print "Title one: " + d['entries'][spot]['title']
-	body = "Body: " + d['entries'][spot]['description']
-	body = "\n".join(body.split("<br />"))
-	print body
-	return
+	choice = ""
+	while choice != "quit":
+		print "starting"
+		print "Feed title: " + d['feed']['title']
+		arts = len(d['entries'])
+		for i in range(0,arts):
+			print "Article " + str(i+1) + ": " + d['entries'][i]['title']
+			time.sleep(5)
+		choice = raw_input("Go again?")
 
+def weathertest(feed, spot):
+	d = feedparser.parse(feed)
+	print "Feed title: " + d['channel']['title']
 def ask():
 	feed = raw_input("Please enter a feed url: ")
 	spot = int(raw_input("Please enter a article number: "))
